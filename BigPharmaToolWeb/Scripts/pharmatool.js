467f2fb40a2875e77df79c4f3b58e2b6f4f4bc66
Lars-Erik Aabech <lars-erik.aabech@markedspartner.no>
2015-09-28 22:12:24 +0200

    Initial commit

---
+++ BigPharmaToolWeb/Scripts/pharmatool.js
@@ -0,0 +1,28 @@
+﻿(function() {
+
+    var module = angular.module("big-pharma-tool", []);
+
+    function applicationController(scope, dataService) {
+        scope.data = {};
+
+        dataService.getEffects().then(function(data) { scope.data = data; });
+    }
+
+    module.controller("app-controller", ["$scope", "data-service", applicationController]);
+
+    function dataServiceImpl(http, q) {
+        
+        this.getEffects = function() {
+            var def = q.defer();
+            http.get("/data/effects.json")
+                .then(function(response) {
+                    def.resolve(response);
+                });
+            return def.promise;
+        }
+    }
+
+    module.service("data-service", ["$http", "$q", dataServiceImpl]);
+
+
+}());
